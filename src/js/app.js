8e4da4ad90be80317e86989d7d9e2275645dba05
Xhamm2010 <46625640+Xhamm2010@users.noreply.github.com>
2024-04-24 23:12:20 +0100

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,69 +1,62 @@
-// Import CSS (assuming this is being used in a web environment)
-// import "../css/style.css";
-
+// Import Web3 and contract library
 const Web3 = require('web3');
 const contract = require('@truffle/contract');
 
+// Import Voting contract artifact
 const votingArtifacts = require('../../build/contracts/Voting.json');
 const VotingContract = contract(votingArtifacts);
 
-const initializeWeb3 = () => {
-  if (typeof web3 !== "undefined") {
-    console.warn("Using web3 detected from external source like Metamask");
-    window.eth = new Web3(window.ethereum);
-  } else {
-    console.warn("No web3 detected. Falling back to http://localhost:9545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for deployment. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
-    window.eth = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"));
+// Initialize Web3 and set contract provider and defaults
+const initializeWeb3 = async () => {
+  try {
+    if (typeof window.ethereum !== "undefined") {
+      console.warn("Using web3 detected from external source like Metamask");
+      window.eth = new Web3(window.ethereum);
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+    } else {
+      console.warn("No web3 detected. Falling back to http://localhost:9545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for deployment. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
+      window.eth = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"));
+    }
+    VotingContract.setProvider(window.eth.currentProvider);
+    VotingContract.defaults({ from: window.eth.accounts[0], gas: 6654755 });
+  } catch (error) {
+    console.error("Error initializing Web3 and contract provider:", error.message);
   }
 };
 
-const setContractProviderAndDefaults = () => {
-  window.ethereum.request({ method: 'eth_requestAccounts' });
-  VotingContract.setProvider(window.ethereum);
-  VotingContract.defaults({ from: window.ethereum.selectedAddress, gas: 6654755 });
-};
-
+// Load account data
 const loadAccountData = () => {
-  App.account = window.ethereum.selectedAddress;
-  $("#accountAddress").html("Your Account: " + window.ethereum.selectedAddress);
+  $("#accountAddress").html("Your Account: " + window.eth.accounts[0]);
 };
 
-// const addCandidateEvent = () => {
-//   $('#addCandidate').click(() => {
-//     const nameCandidate = $('#name').val();
-//     const partyCandidate = $('#party').val();
-//     VotingContract.deployed().then((instance) => {
-//       instance.addCandidate(nameCandidate, partyCandidate).then((result) => {});
-//     });
-//   });
-// };
-
+// Event handlers
 const addCandidateEvent = () => {
   $('#addCandidate').click(() => {
-    console.log('Add candidate button clicked'); // Debug log
+    console.log('Add candidate button clicked');
     const nameCandidate = $('#name').val();
     const partyCandidate = $('#party').val();
     VotingContract.deployed().then((instance) => {
-      console.log('Contract instance obtained'); // Debug log
+      console.log('Contract instance obtained');
       instance.addCandidate(nameCandidate, partyCandidate).then((result) => {
-        console.log('Candidate added', result); // Debug log
+        console.log('Candidate added', result);
       }).catch((err) => {
-        console.error('Error adding candidate:', err.message); // Error log
+        console.error('Error adding candidate:', err.message);
       });
     }).catch((err) => {
-      console.error('Error deploying contract:', err.message); // Error log
+      console.error('Error deploying contract:', err.message);
     });
   });
 };
 
-
 const addDateEvent = () => {
   $('#addDate').click(() => {
-    const startDate = Date.parse(document.getElementById("startDate").value) / 1000;
-    const endDate = Date.parse(document.getElementById("endDate").value) / 1000;
+    const startDate = Date.parse($("#startDate").val()) / 1000;
+    const endDate = Date.parse($("#endDate").val()) / 1000;
     VotingContract.deployed().then((instance) => {
-      instance.setDates(startDate, endDate).then((result) => {
-        console.log("tarihler verildi");
+      instance.setDates(startDate, endDate).then(() => {
+        console.log("Dates set successfully");
+      }).catch((err) => {
+        console.error("Error setting dates:", err.message);
       });
     });
   });
@@ -74,9 +67,9 @@ const displayDates = () => {
     instance.getDates().then((result) => {
       const startDate = new Date(result[0] * 1000);
       const endDate = new Date(result[1] * 1000);
-      $("#dates").text(startDate.toDateString("#DD#/#MM#/#YYYY#") + " - " + endDate.toDateString("#DD#/#MM#/#YYYY#"));
+      $("#dates").text(startDate.toDateString() + " - " + endDate.toDateString());
     }).catch((err) => {
-      console.error("ERROR! " + err.message);
+      console.error("Error fetching dates:", err.message);
     });
   });
 };
@@ -101,7 +94,7 @@ const checkVoteAndEnableButton = () => {
     instance.checkVote().then((voted) => {
       console.log(voted);
       if (!voted) {
-        $("#voteButton").attr("disabled", false);
+        $("#voteButton").prop("disabled", false);
       }
     });
   });
@@ -114,43 +107,42 @@ const vote = () => {
     return;
   }
   VotingContract.deployed().then((instance) => {
-    instance.vote(parseInt(candidateID)).then((result) => {
-      $("#voteButton").attr("disabled", true);
+    instance.vote(parseInt(candidateID)).then(() => {
+      $("#voteButton").prop("disabled", true);
       $("#msg").html("<p>Voted</p>");
       window.location.reload(1);
+    }).catch((err) => {
+      console.error("Error voting:", err.message);
     });
   });
 };
 
-window.App = {
-  eventStart: () => {
-    initializeWeb3();
+// Initialize app
+const initApp = async () => {
+  await initializeWeb3();
+  loadAccountData();
 
-    window.ethereum.request({ method: 'eth_requestAccounts' });
-    setContractProviderAndDefaults();
+  $(document).ready(() => {
+    addCandidateEvent();
+    addDateEvent();
+    displayDates();
 
-    loadAccountData();
-
-    $(document).ready(() => {
-      addCandidateEvent();
-      addDateEvent();
-
-      displayDates();
-
-      VotingContract.deployed().then((instance) => {
-        instance.getCountCandidates().then((countCandidates) => {
-          displayCandidates(countCandidates);
-          window.countCandidates = countCandidates;
-        });
+    VotingContract.deployed().then((instance) => {
+      instance.getCountCandidates().then((countCandidates) => {
+        displayCandidates(countCandidates);
+        window.countCandidates = countCandidates;
       });
-
-      checkVoteAndEnableButton();
     });
-  },
 
+    checkVoteAndEnableButton();
+  });
+};
+
+window.App = {
+  initApp: initApp,
   vote: vote,
 };
 
 window.addEventListener("load", () => {
-  window.App.eventStart();
+  window.App.initApp();
 });
